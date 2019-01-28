d8f182fa849641eb6b5ad09fcd3c754752d4eaf1
arsengit <alkamyan97@gmail.com>
2019-01-28 20:05:20 +0400

    userContext

--- src/index.js
+++ src/index.js
@@ -4,13 +4,19 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { TranslationProvider } from "./context/TranslationContext";
+import {UserProvider} from "./context/ConnectUser"
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <BrowserRouter>
+  <UserProvider>
     <TranslationProvider>
+      
       <App />
+    
+      
     </TranslationProvider>
+    </UserProvider>
   </BrowserRouter>,
   document.getElementById("root")
 );
