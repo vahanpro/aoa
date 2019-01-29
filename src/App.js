9bc474c5d56258a8c58036c5ebaff9d2a77861c1
arsengit <alkamyan97@gmail.com>
2019-01-29 16:08:19 +0400

    enroll Course

--- src/App.js
+++ src/App.js
@@ -10,51 +10,49 @@ import { connectTranslations } from "./context/TranslationContext";
 import StudentProfile from "./components/Student";
 import Contact from "./components/Contact";
 import SignUp from "./components/SignUp";
-import {ConnectToUser} from "./context/ConnectUser"
+import { ConnectToUser } from "./context/ConnectUser";
 import Courses from "./components/Courses";
 
 class App extends Component {
-
-
- render() {
-   return this.props.userIsLoaded && this.props.initialTextLoaded ? (
-     <>
-       <div>
-         {this.props.user ? (
-           <StudentProfile />
-         ) : (
-           <>
-             <Header />
-             <Route
-               path="/"
-               exact
-               render={props => (
-                 <>
-                   <Content />
-                   <Container />
-                 </>
-               )}
-             />
-             <Route path="/contact" component={Contact} />
-             <Route path="/signUp" component={SignUp} />
-             <Route path="/courses" component={Courses} />
-             <Redirect to="/" />
-             <Footer />
-           </>
-         )}
-       </div>
-       {this.props.textLoaded ? null : (
-         <div className="loading">
-           <img src={Loading} alt="Loading" />
-         </div>
-       )}
-     </>
-   ) : (
-     <div className="loading">
-       <img src={Loading} alt="Loading" />
-     </div>
-   );
- }
+  render() {
+    return this.props.userIsLoaded && this.props.initialTextLoaded ? (
+      <>
+        <div>
+          {this.props.user ? (
+            <StudentProfile />
+          ) : (
+            <>
+              <Header />
+              <Route
+                path="/"
+                exact
+                render={props => (
+                  <>
+                    <Content />
+                    <Container />
+                  </>
+                )}
+              />
+              <Route path="/contact" component={Contact} />
+              <Route path="/signUp" component={SignUp} />
+              <Route path="/courses" component={Courses} />
+              <Redirect to="/" />
+              <Footer />
+            </>
+          )}
+        </div>
+        {this.props.textLoaded ? null : (
+          <div className="loading">
+            <img src={Loading} alt="Loading" />
+          </div>
+        )}
+      </>
+    ) : (
+      <div className="loading">
+        <img src={Loading} alt="Loading" />
+      </div>
+    );
+  }
 }
 
 export default connectTranslations(ConnectToUser(App));
